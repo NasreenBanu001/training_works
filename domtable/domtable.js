34f547bc82480e64b57b058ba8f9c52701a926a0
NasreenBanu001 <s.nasreenbanu001@gmail.com>
2023-07-18 14:51:18 +0530

    Sharing

---
+++ domtable/domtable.js
@@ -0,0 +1,27 @@
+function detail1() {
+    let FirstName = document.getElementById("fname1").innerHTML
+    let LastName=document.getElementById("lname1").innerHTML
+    let Age=document.getElementById("age1").innerHTML
+    alert(`This is ${FirstName} ${LastName} of age ${Age}`);
+}
+
+function detail2() {
+    let FirstName=document.getElementById("fname2").innerHTML
+    let LastName=document.getElementById("lname2").innerHTML
+    let Age=document.getElementById("age2").innerHTML
+    alert(`This is ${FirstName} ${LastName} of age ${Age}`);
+}
+
+function detail3() {
+    let FirstName=document.getElementById("fname3").innerHTML
+    let LastName=document.getElementById("lname3").innerHTML
+    let Age=document.getElementById("age3").innerHTML
+    alert(`This is ${FirstName} ${LastName} of age ${Age}`);
+}
+
+function detail4() {
+    let FirstName=document.getElementById("fname4").innerHTML
+    let LastName=document.getElementById("lname4").innerHTML
+    let Age=document.getElementById("age4").innerHTML
+    alert(`This is ${FirstName} ${LastName} of age ${Age}`);
+}
